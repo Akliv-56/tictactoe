22574c858e03b7d844e7a713f6d38d05f7e1eafc
Artem Klivniak <klivnaka11@gmail.com>
2025-09-15 20:38:54 -0400

    Сделал функцию can_move

--- src/gameplay.py
+++ src/gameplay.py
@@ -19,3 +19,33 @@ class Cell:
 
     def is_empty(self) -> bool:
         return self.mark == EMPTY
+
+
+class Field:
+
+
+    def __init__(self):
+        self.size_x = 3
+        self.size_y = 3
+        self.cells = []
+
+    def build(self):
+        # TODO: Do home
+        pass
+
+    def make_move(self, x:int, y:int, mark:int) -> None:
+        # TODO: Do home
+        pass
+
+    def can_move(self, x: int, y: int) -> bool:
+        if not x <= 3: raise ValueError("")
+        if not y <= 3: raise ValueError("")
+
+        if Cell(x,y).is_empty == True:
+            return True
+        else:
+            return False
+        
+    def clear(self) -> None:
+        # TODO: Do home
+        pass
